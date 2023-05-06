7ec5632d93da5c10c7ad1dec689c1f561c8b2747
Long Nguyen <me@long-nguyen.dev>
2023-05-06 02:11:55 -0500

    Start basic C++ env

--- flake.nix
+++ flake.nix
@@ -10,9 +10,28 @@
         flake-utils.lib.eachDefaultSystem (system:
         let 
             pkgs = import nixpkgs { inherit system; };
+            llvm = pkgs.llvmPackages_15;
+            lib = nixpkgs.lib;
         in rec {
             devShells.default = pkgs.mkShell {
                 packages = [ 
+                    pkgs.cmake
+
+                    # debugger
+                    llvm.lldb
+
+                    # XXX: the order of include matters
+                    pkgs.clang-tools
+                    llvm.clang # clangd
+                    llvm.libcxx
+
+                    pkgs.gtest
+                ];
+
+                CXXFLAGS = "-std=c++20";
+                CPATH = builtins.concatStringsSep ":" [
+                    (lib.makeSearchPathOutput "dev" "include" [ llvm.libcxx ])
+                    (lib.makeSearchPath "resource-root/include" [ llvm.clang ])
                 ];
 
                 shellHook = ''
